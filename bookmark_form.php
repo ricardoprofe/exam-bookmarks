428f18d99120238b1592ab3e36b41fbf41d20fa3
Ricardo <ricardo.profe.informatica@gmail.com>
2022-11-21 12:47:36 +0100

    first commit

---
+++ bookmark_form.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> New Bookmark </title>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <link type="text/css" rel="stylesheet" href="main.css">
+</head>
+
+<body>
+<h1> New bookmark </h1>
+<form> <!-- Complete the post and method attributes -->
+    <label>ID</label>
+    <input type="text" name="id" readonly value=""> <br>
+
+    <label>Web name</label>
+    <input type="text" name="name" value="" size="40"> <br>
+
+    <label>URL</label>
+    <input type="text" name="url" value="" size="80"> <br>
+
+    <label>Timestamp</label>
+    <input type="text" name="timestamp" value=""> <br>
+
+    <input type="submit" name="submit" value="Save">
+    <input type="submit" name="import" value="Import CSV">
+</form>
+
+</body>
+</html>
---
+++ bookmarks_list.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Bookmarks list </title>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <link type="text/css" rel="stylesheet" href="main.css">
+</head>
+
+<body>
+<h1> Bookmarks list </h1>
+<form  style="border: none"> <!-- Complete the form attributes -->
+    <p class="centered">
+        <button type='submit' form='form1' name='new' > Create new bookmark </button>
+    </p>
+</form>
+
+<table>
+    <tr>
+        <th>ID</th> <th>Web name</th> <th>URL</th> <th>Timestamp</th>
+    </tr>
+    <!-- Complete the table form the database -->
+</table>
+
+
+</body>
+</html>
